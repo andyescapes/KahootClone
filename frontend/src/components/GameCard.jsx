6bcb5f6f68982d97724ffefca5cf4e9b48c920c7
Andrew Zhao <andrew_zhao14@live.com>
2021-04-22 15:19:41 +1000

    assignment donev2

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -93,15 +92,6 @@ function GameCard (props) {
               : history.push(`/edit/${props.id}/${props.questionId}`)
           }
         >
-          {props.thumbnail ?? (
-            <CardMedia
-
-              component="img"
-              height="140"
-              image={props.thumbnail}
-              title="Contemplative Reptile"
-            />
-          )}
 
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
